f3826c6751bc6f536d23dc79323d1088ed69457e
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2023-01-26 23:50:16 +0530

    Today's React Practice

--- Front End Development Libraries/React/script.js
+++ Front End Development Libraries/React/script.js
@@ -1,3 +1,237 @@
 const JSX = <div>
     <p></p>
 </div>
+/*********************************************************************************** */
+
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    componentWillMount() {
+        // Change code below this line
+        console.log("Message before Mount");
+        // Change code above this line
+    }
+    render() {
+        return <div />
+    }
+};
+
+/*********************************************************************************** */
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeUsers: null
+        };
+    }
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                activeUsers: 1273
+            });
+        }, 2000);
+    }
+    render() {
+        return (
+            <div>
+                {/* Change code below this line */}
+                <h1>Active Users: {this.state.activeUsers} </h1>
+                {/* Change code above this line */}
+            </div>
+        );
+    }
+}
+/*********************************************************************************** */
+
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: ''
+        };
+        this.handleEnter = this.handleEnter.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+    }
+    // Change code below this line
+    componentDidMount() {
+        document.addEventListener('keydown', this.handleKeyPress);
+    }
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.handleKeyPress);
+    }
+    // Change code above this line
+    handleEnter() {
+        this.setState((state) => ({
+            message: state.message + 'You pressed the enter key! '
+        }));
+    }
+    handleKeyPress(event) {
+        if (event.keyCode === 13) {
+            this.handleEnter();
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.message}</h1>
+            </div>
+        );
+    }
+};
+
+/*********************************************************************************** */
+
+class OnlyEvens extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('Should I update?');
+        // Change code below this line
+        if (nextProps.value % 2 == 0)
+            return true;
+        else
+            return false;
+        // Change code above this line
+    }
+    componentDidUpdate() {
+        console.log('Component re-rendered.');
+    }
+    render() {
+        return <h1>{this.props.value}</h1>;
+    }
+}
+
+class Controller extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 0
+        };
+        this.addValue = this.addValue.bind(this);
+    }
+    addValue() {
+        this.setState(state => ({
+            value: state.value + 1
+        }));
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.addValue}>Add</button>
+                <OnlyEvens value={this.state.value} />
+            </div>
+        );
+    }
+}
+
+/*********************************************************************************** */
+
+class Colorful extends React.Component {
+    render() {
+        return (
+            <div style={{ color: "red", fontSize: 72 }}>Big Red</div>
+        );
+    }
+};
+
+/*********************************************************************************** */
+
+const styles = {
+    color: "purple",
+    fontSize: 40,
+    border: "2px solid purple"
+}
+// Change code above this line
+class Colorful extends React.Component {
+    render() {
+        // Change code below this line
+        return (
+            <div style={styles}>Style Me!</div>
+        );
+        // Change code above this line
+    }
+};
+
+/*********************************************************************************** */
+
+const inputStyle = {
+    width: 235,
+    margin: 5
+};
+
+class MagicEightBall extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userInput: '',
+            randomIndex: ''
+        };
+        this.ask = this.ask.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    ask() {
+        if (this.state.userInput) {
+            this.setState({
+                randomIndex: Math.floor(Math.random() * 20),
+                userInput: ''
+            });
+        }
+    }
+    handleChange(event) {
+        this.setState({
+            userInput: event.target.value
+        });
+    }
+    render() {
+        const possibleAnswers = [
+            'It is certain',
+            'It is decidedly so',
+            'Without a doubt',
+            'Yes, definitely',
+            'You may rely on it',
+            'As I see it, yes',
+            'Outlook good',
+            'Yes',
+            'Signs point to yes',
+            'Reply hazy try again',
+            'Ask again later',
+            'Better not tell you now',
+            'Cannot predict now',
+            'Concentrate and ask again',
+            "Don't count on it",
+            'My reply is no',
+            'My sources say no',
+            'Most likely',
+            'Outlook not so good',
+            'Very doubtful'
+        ];
+        const answer = possibleAnswers[this.state.randomIndex]; // Change this line
+        return (
+            <div>
+                <input
+                    type='text'
+                    value={this.state.userInput}
+                    onChange={this.handleChange}
+                    style={inputStyle}
+                />
+                <br />
+                <button onClick={this.ask}>Ask the Magic Eight Ball!</button>
+                <br />
+                <h3>Answer:</h3>
+                <p>
+                    {/* Change code below this line */}
+                    {answer}
+                    {/* Change code above this line */}
+                </p>
+            </div>
+        );
+    }
+}
+
+/*********************************************************************************** */
+
+
+
+/*********************************************************************************** */
