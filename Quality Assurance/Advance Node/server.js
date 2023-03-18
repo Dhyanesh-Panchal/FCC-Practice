ce026aa0acf7f417ff008803fdae4ff5d4eac34c
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2023-03-18 23:42:01 +0530

    Advance Node Module 50% completed

--- Quality Assurance/Advance Node/server.js
+++ Quality Assurance/Advance Node/server.js
@@ -6,8 +6,23 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const path = require('path');
 const session = require('express-session');
 const passport = require('passport');
+const { ObjectID } = require('mongodb');
+const LocalStrategy = require('passport-local');
 
 const app = express();
+const ensureAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  else {
+    res.redirect('/')
+  }
+}
+
+
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -20,15 +35,75 @@ app.use(session({
   cookie: { secure: false }
 }))
 app.use(passport.initialize());
-app.use(passport.session())
+app.use(passport.session());
 
 
-app.set('view engine', 'pug');
-app.set('views', './views/pug')
 
-app.route('/').get((req, res) => {
-  res.render('index', { title: 'Hello', message: 'Please log in' });
-});
+myDB(async (client) => {
+  const myDatabase = await client.db('fcc-advance-node').collection('users');
+
+
+  app.route('/').get((req, res) => {
+    console.log('Database Connected Succesfully!')
+    res.render('index', { title: 'Connected to Database', message: 'Please login', showLogin: true });
+  })
+
+  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+    console.log(`User ${req.user.username} is Authenticated!`);
+    res.redirect('/profile');
+  })
+
+  app.route('/profile').get(ensureAuthenticated, (req, res) => {
+    res.render('profile', { username: req.user.username });
+  })
+
+  app.route('/logout').get((req, res) => {
+    req.logOut();
+    res.redirect('/')
+  })
+
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found')
+  })
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  })
+
+  passport.deserializeUser((id, done) => {
+    myDatabase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    })
+    // done(null, null);
+  })
+
+  passport.use(new LocalStrategy((username, password, done) => {
+    myDatabase.findOne({ username: username }, (err, user) => {
+      console.log(`User ${username} attemped to login`);
+      if (err) {
+        return done(err);
+      }
+      else if (!user) {
+        console.log('User dont exist');
+        return done(null, false);
+      }
+      else if (password != user.password) {
+        console.log('Incorrect password');
+        return done(null, false);
+      }
+      else {
+        return done(null, user);
+      }
+    })
+  }))
+}).catch(e => {
+  app.render('index', { title: e, message: 'Unable to connect to database' });
+})
+
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
